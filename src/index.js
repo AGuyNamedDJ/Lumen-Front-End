78d816b35c70f61001bcb4575cffa6a414d6e079
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2024-07-09 14:42:46 -0500

    Initialize basic structure and setup for Lumen Front-End project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Imports
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+
+// Import: Utilities
+
+
+// Import: Pages
+
+// Brower Router
+
+
+const app = document.getElementById("apps")
+const root = createRoot(app)
+root.render(<RouterProvider router={router} />)
