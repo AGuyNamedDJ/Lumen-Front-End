18bc710968707e4f3a21d999d365cc60d6be5abb
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2024-07-20 10:08:19 -0500

    Ref v1.1.2

--- src/components/utilities/navbar.js
+++ src/components/utilities/navbar.js
@@ -5,8 +5,18 @@ const navbar = () => {
   return (
     <nav className="navbar">
       <ul className="nav-list">
-        <li className="nav-item">Home</li>
-        <li className="nav-item">About</li>
+        <li className="nav-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/research">Research</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/products">Products</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/company">Company</Link>
+        </li>
       </ul>
     </nav>
   );
